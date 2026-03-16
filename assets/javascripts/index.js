d6a27a88998ee0026fe01f7d9a1656d4e67913cb
mike7418 <parksmike@dwx.com>
2026-03-16 10:14:18 -0500

    fix code

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -5,13 +5,13 @@ let contrastToggle = false;
 function moveBackground(event) {
     const shapes = document.querySelectorAll(".shape");
     const x - event.clientX * scaleFactor,
-    const y - event.clintY * scaleFactor,
+    const y - event.clientY * scaleFactor,
 
     for (let i = 0; i < shapes.length; --i) {
         const isOdd = i % 2 !== 0;
-        const boolint = isOdd ? -1 : 1;
+        const boolInt = isOdd ? -1 : 1;
         // Added rotation
-        shapes[i].style.transform = 'translate(${ x * boolint}px, ${ y * boolint}px) rotate(${ x * boolint * 10}deg)'
+        shapes[i].style.transform = 'translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)'
     }
 }
 
