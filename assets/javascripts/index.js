18960cde0ae87955e3155ab5dea16237975099ee
mike7418 <parksmike@dwx.com>
2026-03-16 12:17:51 -0500

    finish index,js code

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -18,3 +18,36 @@ function moveBackground(event) {
 function toggleContrast() {
     document.body.classList.toggle('dark-mode');
 }
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            "service_80ih0if",
+            "template_d9refyl",
+            event.target,
+            "user_K1PoFs8pB2YVWStDxrUls"
+        )
+        .then(() => {
+            loading.classList.remove("modal__overlay--loading");
+            success.classList += " modal__overlay--visible";
+        })
+        .catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+            );
+        });
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList..remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
